b5a59e2915af8d7da76ca04619761e6205a0c44d
Rahman Younus <rahman.younus@dazn.com>
2019-11-26 10:28:18 +0000

    got api gateway working - now invokes lambda function

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -1,24 +1,17 @@
+resource "aws_api_gateway_rest_api" "terraform-api-gateway-rest-api" {
+  name        = "terraform-api-gateway"
+  description = "Terraform Api Gateway for Lambda"
+}
+
 resource "aws_api_gateway_resource" "terraform-api-gateway" {
   rest_api_id = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
   parent_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.root_resource_id}"
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_rest_api" "terraform-api-gateway-rest-api" {
-  name        = "terraform-api-gateway"
-  description = "Terraform Api Gateway for Lambda"
-}
-
 resource "aws_api_gateway_method" "terraform-api-gateway-method" {
   rest_api_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
   resource_id   = "${aws_api_gateway_resource.terraform-api-gateway.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "terraform-api-gateway-root-method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.root_resource_id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
@@ -33,9 +26,16 @@ resource "aws_api_gateway_integration" "terraform-api-gateway-integration" {
   uri                     = "${aws_lambda_function.terraform-lambda.invoke_arn}"
 }
 
+resource "aws_api_gateway_method" "terraform-api-gateway-root-method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "terraform-api-gateway-root-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
-  resource_id = "${aws_api_gateway_method.terraform-api-gateway.resource_id}"
+  resource_id = "${aws_api_gateway_method.terraform-api-gateway-root-method.resource_id}"
   http_method = "${aws_api_gateway_method.terraform-api-gateway-root-method.http_method}"
 
   integration_http_method = "POST"
@@ -50,5 +50,5 @@ resource "aws_api_gateway_deployment" "terraform-api-gateway-deployment" {
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.terraform-api-gateway-rest-api.id}"
-  stage_name  = "dev"
+  stage_name  = "test"
 }
