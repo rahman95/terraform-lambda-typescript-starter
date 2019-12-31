141f47ece2e6c24ada802e8197f675824ba97877
Rahman Younus <rahman.younus@dazn.com>
2019-12-31 17:16:05 +0000

    updated terraform files

---
+++ terraform/iam.tf
@@ -0,0 +1,52 @@
+data "aws_iam_policy_document" "lambda_assume_role_document" {
+  version = "2012-10-17"
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy_document" "lambda_document" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "cloudwatch:PutMetricData",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  policy = "${data.aws_iam_policy_document.lambda_document.json}"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${local.name}-role"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role_document.json}"
+
+  tags = "${local.tags}"
+}
+
+resource "aws_iam_policy_attachment" "lambda_attachment" {
+  name = "${local.name}-attachment"
+
+  roles = [
+    "${aws_iam_role.lambda_role.name}",
+  ]
+
+  policy_arn = "${aws_iam_policy.lambda_policy.arn}"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,4 @@
-output "s3" {
-  value       = "${aws_s3_bucket.terraform-s3-lambda.id}"
-  description = "The public IP of the web server"
-}
-
-output "s3-object-id" {
-  value       = "${aws_s3_bucket_object.terraform-s3-object.id}"
-  description = "The public IP of the web server"
-}
-
-output "s3-object-key" {
-  value       = "${aws_s3_bucket_object.terraform-s3-object.key}"
-  description = "The public IP of the web server"
-}
-
 output "api_base_url" {
-  value       = "${aws_api_gateway_deployment.terraform-api-gateway-deployment.invoke_url}"
+  value       = "${aws_api_gateway_deployment.api_gateway_deployment.invoke_url}"
   description = "The public IP of the API"
 }
